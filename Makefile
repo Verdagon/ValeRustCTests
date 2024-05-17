211e1ce7d493ed09e4206e8e06b5712f98701ca0
Evan Ovadia <verdagon_null@verdagon.dev>
2024-05-17 10:44:46 -0400

    Successfully creates a directory!

---
+++ Makefile
@@ -0,0 +1,30 @@
+CC = clang++
+EXAMPLE_PROJECT_DIR := /Volumes/V/CCatter/rust/target/aarch64-apple-darwin/debug
+export PKG_CONFIG_PATH := $(EXAMPLE_PROJECT_DIR):$(PKG_CONFIG_PATH)
+
+CFLAGS = -Wall -Wextra -g
+INCLUDES = `pkg-config --cflags --static rust_deps`
+LIBS = `pkg-config --libs --static rust_deps`
+
+all:	build ccatter
+
+build:
+	mkdir -p build
+
+ccatter: build/main.o
+	echo $(CC) $(CFLAGS) $(INCLUDES) $^ -o $@ $(LIBS)
+	$(CC) $(CFLAGS) $(INCLUDES) $^ -o build/ccatter $(LIBS)
+
+build/main.o:	src/main.c rust
+	@echo "Invoking ValeRuster"
+	@grep '^#pragma vrinclude' src/main.c | /Volumes/V/Vale/ValeRuster/target/debug/ValeRuster --crate std --cargo_toml Dependencies.toml instantiate --output_dir rust
+	@echo "Compiling the C file..."
+	echo $(CC) $(CFLAGS) $(INCLUDES) -I . -c $< -o $@
+	$(CC) $(CFLAGS) $(INCLUDES) -I . -c $< -o $@
+
+rust:
+	mkdir rust
+
+.PHONY: clean
+clean:
+	rm -rf *.o *.d *.h rust build
---
+++ src/main.c
@@ -0,0 +1,30 @@
+#pragma vrinclude std::path::PathBuf as RustPathBuf
+#pragma vrinclude std::io::Result<()> as RustIoResultTup
+// #pragma vrinclude std::io::Result<()>::is_ok as RustIoResultTupIsOk
+#pragma vrinclude std::string::String as RustString
+#pragma vrinclude std::string::String::new as RustStringNew
+#pragma vrinclude std::string::String::push as RustStringPush
+#pragma vrinclude std::path::PathBuf::new as RustPathBufNew
+#pragma vrinclude std::path::PathBuf::push::<&std::string::String> as RustPathBufPushStr
+#pragma vrinclude std::fs::create_dir<&std::path::PathBuf> as RustCreateDir
+
+#include <rust_deps/rust_deps.h>
+#include <stdio.h>
+
+int main() {
+  RustPathBuf path_buf = RustPathBufNew();
+
+  RustString str = RustStringNew();
+  RustStringPush(&str, 'x');
+
+  RustPathBufPushStr(&path_buf, &str);
+
+  RustCreateDir(&path_buf);
+  // if (RustIoResultTupIsOk(&result)) {
+    printf("Success!\n");
+    return 0;
+  // } else {
+  //   printf("Error creating directory!\n");
+  //   return 1;
+  // }
+}
