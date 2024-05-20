6e25541d159b4da3239814d7289d8f559e86b19a
Evan Ovadia <verdagon_null@verdagon.dev>
2024-05-20 18:35:14 -0400

    Successfully calls drop!

--- src/mkdir.c
+++ src/mkdir.c
@@ -1,30 +1,39 @@
 #pragma rsuse PathBuf = std::path::PathBuf
 #pragma rsuse IoResultTup = std::io::Result<()>
-#pragma rsuse IoResultTupIsOk = IoResultTup::is_ok
+#pragma rsuse IoResultTup_is_ok = IoResultTup::is_ok
+#pragma rsuse IoResultTup_drop = IoResultTup::drop
 #pragma rsuse String = std::string::String
-#pragma rsuse StringNew = String::new
-#pragma rsuse StringPush = String::push
-#pragma rsuse PathBufNew = PathBuf::new
-#pragma rsuse PathBufPushStr = PathBuf::push::<&String>
-#pragma rsuse CreateDir = std::fs::create_dir<&PathBuf>
-
+#pragma rsuse String_new = String::new
+#pragma rsuse String_drop = String::drop
+#pragma rsuse String_push = String::push
+#pragma rsuse PathBuf_new = PathBuf::new
+#pragma rsuse PathBuf_pushStr = PathBuf::push::<&String>
+#pragma rsuse PathBuf_drop = PathBuf::drop
+#pragma rsuse create_dir = std::fs::create_dir<&PathBuf>
 #include <rust_deps/rust_deps.h>
+
 #include <stdio.h>
 
 int main() {
-  PathBuf path_buf = PathBufNew();
+  PathBuf path_buf = PathBuf_new();
 
-  String str = StringNew();
-  StringPush(&str, 'x');
+  String str = String_new();
+  String_push(&str, 'x');
 
-  PathBufPushStr(&path_buf, &str);
+  PathBuf_pushStr(&path_buf, &str);
 
-  IoResultTup result = CreateDir(&path_buf);
-  if (IoResultTupIsOk(&result)) {
+  String_drop(&str);
+
+  IoResultTup result = create_dir(&path_buf);
+  PathBuf_drop(&path_buf);
+
+  if (IoResultTup_is_ok(&result)) {
     printf("Success!\n");
+    IoResultTup_drop(&result);
     return 0;
   } else {
     printf("Error creating directory!\n");
+    IoResultTup_drop(&result);
     return 1;
   }
 }
--- src/strlen.c
+++ src/strlen.c
@@ -1,13 +1,16 @@
+#pragma rsuse Str = &str
 #pragma rsuse String = std::string::String
-#pragma rsuse StringFromStr = String::From<&str>::from
-#pragma rsuse StringLen = String::len
+#pragma rsuse String_fromStr = String::From<&str>::from
+#pragma rsuse String_len = String::len
+#pragma rsuse String_drop = String::drop
 #include <rust_deps/rust_deps.h>
 #include <stdio.h>
 
 int main() {
-  VR_str_ref constant = StrFromCStr("bork");
-  String str = StringFromStrRef(constant);
-  int len = StringLen(&str);
+  Str b = VR_StrFromCStr("bork");
+  String s = String_fromStr(b);
+  int len = String_len(&s);
   printf("Length: %d\n", len);
+  String_drop(&s);
   return 0;
 }
